299c3f083f627fd412eb9cf7e1d9a64cfc0f20d7
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-05 18:48:47 +0200

    implemented img change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const images = document.querySelectorAll('img');
+const mainImg = document.querySelector('#largeImg');
+
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const img = e.currentTarget.getAttribute('src');
+
+    mainImg.src = img;
+  });
+});
