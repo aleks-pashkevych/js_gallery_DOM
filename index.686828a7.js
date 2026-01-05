d286dad1c468ece4770d4a5c0adf04365f1f19a8
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-05 19:11:00 +0200

    make build

---
+++ index.686828a7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.getAttribute("src");e.src=r});
+//# sourceMappingURL=index.686828a7.js.map
